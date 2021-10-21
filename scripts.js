64eed3fe000576ac3f4c311942418a9cd94c7d30
davinbrown <davinbrown6@gmail.com>
2021-10-21 11:57:23 -0500

    Added newsletter functionality using javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+
+window.onload = function() {
+    document.querySelector("button").addEventListener("click", function(event){
+        event.preventDefault()
+        subscribe();
+    });
+};
+
+function subscribe() {
+    // get the value from email input and remove remove whitespace from both sides
+    var email = document.getElementById("email").value.trim();
+    if (email == "") {
+        document.querySelector(".message").innerHTML = "Please enter a valid email address";            
+    }else{
+        document.querySelector(".message").innerHTML = "Thank you! Your email address " + email + " has been added to our mailing list!";            
+    }
+}
